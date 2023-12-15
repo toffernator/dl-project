fb014067d9b01678c03073d70d3df100f97eb2be
Christoffer Gram <christofferkgram@gmail.com>
2023-12-15 13:56:20 +0100

    Initialize Poetry & poetry2nix

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
 # DL Project
 
 ## Setting up
+
+First get the dataset placed in the correct place:
+
 1. Copy the provided dataset file `Final Project data.zip` to the main directory
 2. Run `setup_dataset.sh`, or
 3. Unzip the zip file and rename the folder `Final Project data` to `dataset`
+
+Next, make sure that [Poetry](https://python-poetry.org/docs/#installation) is installed:
+
+```sh
+pipx install poetry==1.7.1
+```
+
+Then you run to get started
+
+```sh
+poetry install
+poetry run main
+```
+
+Your final direcotry structure should look like:
+
+```
+.
+├── dataset
+│   ├── Cross
+│   │   ├── test1
+│   │   ├── test2
+│   │   ├── test3
+│   │   └── train
+│   └── Intra
+│       ├── test
+│       └── train
+├── docs
+└── src
+```
+
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Deep Learning Project";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          src = mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.src;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.src ];
+          packages = [ pkgs.poetry ];
+        };
+      });
+}
