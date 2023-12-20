5e7902a1b954b1fdde71ad8b770f5d9fa8cc4a53
Faiz Ilham <faizilham.faizz@gmail.com>
2023-12-20 15:29:58 +0100

    add scipy

--- src/downsampling.py
+++ src/downsampling.py
@@ -1,17 +1,18 @@
 import os
 import numpy as np
+from scipy.signal import resample, decimate
 
-class Downsampler():
+
+class Downsampler:
     # def __init__(self, data):
     #     ...
 
-
     def applyDownSampling(data, downsampling_factor):
-        new_column_size = int(data.shape[1]*downsampling_factor)
-        downsampled_data = data[:, ::int(1/downsampling_factor)]
+        new_column_size = int(data.shape[1] * downsampling_factor)
+        downsampled_data = data[:, :: int(1 / downsampling_factor)]
         return downsampled_data, new_column_size
-    
-    def chunk_and_average_columns(self ,data, chunk_size):
+
+    def chunk_and_average_columns(self, data, chunk_size):
         num_chunks = data.shape[1] // chunk_size
         averaged_chunks = np.zeros((data.shape[0], num_chunks))
 
@@ -23,3 +24,6 @@ class Downsampler():
             averaged_chunks[:, i] = np.mean(chunk, axis=1)
 
         return averaged_chunks
+
+    def decimate(self, data, factor):
+        return decimate(data, factor, axis=1)
